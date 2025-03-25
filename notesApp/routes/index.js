d1f652e801d3e024b6153b0ff62bdc56f6b72bee
Sarah Winne <sarahwinne@Sarahs-Air-4.houghton.edu>
2025-03-25 08:46:55 -0400

    Work on Notes App

--- notesApp/routes/index.js
+++ notesApp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+// const { redirect } = require('express/lib/response');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express JS Website' });
 });
 
 module.exports = router;
