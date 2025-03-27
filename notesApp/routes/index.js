8e05a043d6b1096148c8b93d3f7a0e95a5655125
Sarah Winne <sarahwinne@Sarahs-Air-4.houghton.edu>
2025-03-27 04:56:36 -0400

    Work on notes app

--- notesApp/routes/index.js
+++ notesApp/routes/index.js
@@ -3,27 +3,27 @@ var express = require('express');
 var router = express.Router();
 
 let note = [
-  {id: "(unique identifier, can be a simple number or string)", 
+  {id: 0, 
     title: "Getting Started", 
     body: "You're just getting started!", 
     color: "white", 
     starred: true, 
-    createdAt: "(timestamp for when the note was created)", 
-    updatedAt: "(timestamp for when the note was last updated)"
+    createdAt: 0, 
+    updatedAt: 0
     }
 ]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Notes', data: note});
+  res.render('index', { title: 'Notes', data: note, currentDate: Date.now()});
 });
 
 router.get('/notes/new', function(req, res, next) {
-  res.render('new', { title: 'Notes', data: note});
+  res.render('new', { title: 'Notes', data: note, currentDate: Date.now()});
 });
 
 router.get('/notes/:id/edit', function(req, res, next) {
-  res.render('edit', { title: 'Notes', data: note});
+  res.render('edit', { title: 'Notes', data: note, currentDate: Date.now()});
 });
 
 router.get('/notes/:id/star', function(req, res, next) {
@@ -32,26 +32,42 @@ router.get('/notes/:id/star', function(req, res, next) {
   if(thisNote==null){
     res.redirect('/');
   } else {
-    res.render('index', { title: 'Notes', data: thisNote});
+    res.render('index', { title: 'Notes', data: thisNote, currentDate: Date.now()});
   }
 });
 
 router.post('/notes', function(req, res, next) {
-  // var id= req.body.fname;
+  var id= req.body.updatedAt;
   var title= req.body.title;
   var body= req.body.body;
   var color= req.body.color;
   var starred= req.body.starred;
-  var createdAt= req.body.lname;
-  // var updatedAt= req.body.lname;
-  var newNote = {id:createdAt, title:title, body:body, color:color, starred:starred, createdAt:createdAt, updatedAt:createdAt}
+  var createdAt= req.body.updatedAt;
+  var updatedAt= req.body.updatedAt;
+  var newNote = {id:id, title:title, body:body, color:color, starred:starred, createdAt:createdAt, updatedAt:updatedAt}
   note.push(newNote);
-  res.render('new', { title: 'Notes', data: note});
+  res.render('index', { title: 'Notes', data: note, currentDate: Date.now()});
+});
+
+router.post('/notes/:id/edit', function(req, res, next) {
+  var id= req.params.id;
+  var title= req.body.title;
+  var body= req.body.body;
+  var color= req.body.color;
+  var starred= req.body.starred;
+  var createdAt= req.body.id;
+  var updatedAt= req.body.updatedAt;
+  // note = note.filter(thisNote => thisNote.id !== id); 
+  var newNote = {id:id, title:title, body:body, color:color, starred:starred, createdAt:createdAt, updatedAt:updatedAt}
+  note.push(newNote);
+  res.render('index', { title: 'Notes', data: note, currentDate: Date.now()});
 });
 
 router.post('/notes/:id', function(req, res, next) {
-
-  res.render('edit', { title: 'Notes', data: note});
+  var id= req.params.id;
+  // Removes the object with firstname: id
+  note = note.filter(notes => notes.createdAt !== id); 
+  res.render('index', { title: 'Notes', data: note, currentDate: Date.now()});
 });
 
 
